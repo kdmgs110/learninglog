b9e003bb777c8534b05e9ccc0c5b7f53d86cd669
Dai <kdmgs110@gmail.com>
2016-10-14 10:06:18 +0000

    set dialogue to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,6 +15,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  before_action :set_dialogue
+  
   protected
 
   def configure_permitted_parameters
