5309d61e4cec3534fab626bd826f864ecf7414f7
Dai <kdmgs110@gmail.com>
2016-10-14 11:26:30 +0000

    precomplied

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,6 @@ module Workspace
     
     
     #codes bollow for ckeitor 
-    
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
