3470c1320664d0949a8c6e8e1da61af4e6a2e35b
Takehiro Mouri <takehiromouri@gmail.com>
2016-10-17 12:24:16 +0545

    code review

--- app/controllers/dialogues_controller.rb
+++ app/controllers/dialogues_controller.rb
@@ -5,8 +5,11 @@ class DialoguesController < ApplicationController
     end
     
     def create
+        ## ここ@users設定うる必要ないかも,current_userでおけ
         @users = current_user
+
         @dialogues = @users.dialogues.create(dialogue_params.merge(user_id: current_user.id))
+        ## @users.dialoguesでcreateしてるからuser_idをmergeする必要ないー自動的にuser_idはcurrent_user.idに設定される
         if @dialogues.valid?
         redirect_to root_path
         else
